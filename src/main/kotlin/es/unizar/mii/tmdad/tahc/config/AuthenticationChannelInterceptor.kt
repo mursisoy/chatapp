2a632745e601e409367a1a9b36e4bf822018da37
Alberto Mur Lopez <amur@unizar.es>
2023-04-24 23:33:24 +0200

    STOMP authentication with securityContextChannelInterceptor

--- src/main/kotlin/es/unizar/mii/tmdad/tahc/config/AuthenticationChannelInterceptor.kt
+++ src/main/kotlin/es/unizar/mii/tmdad/tahc/config/AuthenticationChannelInterceptor.kt
@@ -1,50 +1,44 @@
 package es.unizar.mii.tmdad.tahc.config
 
+import es.unizar.mii.tmdad.tahc.service.impl.JwtServiceImpl
 import org.slf4j.LoggerFactory
 import org.springframework.messaging.Message
 import org.springframework.messaging.MessageChannel
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor
 import org.springframework.messaging.simp.SimpMessageType
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor
 import org.springframework.messaging.support.ChannelInterceptor
 import org.springframework.messaging.support.MessageBuilder
+import org.springframework.messaging.support.MessageHeaderAccessor
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
 
 
 @Component
-class ChatPreHandler(private val jwtService: JwtService) : ChannelInterceptor {
+class AuthenticationChannelInterceptor(private val jwtServiceImpl: JwtServiceImpl) : ChannelInterceptor {
     private val logger = LoggerFactory.getLogger(javaClass)
     override fun preSend(message: Message<*>, channel: MessageChannel): Message<*>? {
-        val headerAccessor = StompHeaderAccessor.wrap(message)
-        logger.info(headerAccessor.messageType?.name)
+        val headerAccessor = MessageHeaderAccessor.getAccessor(message) as StompHeaderAccessor
+        SimpMessageHeaderAccessor.getSessionAttributes(message.headers)
         if (headerAccessor.messageType?.equals(SimpMessageType.CONNECT) == true) {
-            val authHeader = headerAccessor.getNativeHeader("Authorization")?.get(0);
+            val authHeader = headerAccessor.getNativeHeader("Authorization")?.get(0)
             logger.info(authHeader)
             if (authHeader == null || !authHeader.startsWith("Bearer ")) {
                 return message
             }
             val jwt: String = authHeader.substring(7)
-            val username = jwtService.extractSubject(jwt)
-            logger.info(username)
-            val userDetails: UserDetails? = jwtService.extractUser(jwt)
-            logger.info(userDetails.toString())
-            if (userDetails != null && username!!.isNotEmpty() && SecurityContextHolder.getContext().authentication == null) {
-//            val userDetails: UserDetails = userDetailService.loadUserByUsername(userEmail)
-                if (jwtService.isTokenValid(jwt, userDetails)) {
+            val username = jwtServiceImpl.extractSubject(jwt)
+            val userDetails: UserDetails? = jwtServiceImpl.extractUser(jwt)
+            if (userDetails != null && username!!.isNotEmpty()) {
+                if (jwtServiceImpl.isTokenValid(jwt, userDetails)) {
                     val authToken = UsernamePasswordAuthenticationToken(
-                        userDetails,
+                        username,
                         null,
-                        userDetails.authorities
+                        userDetails.authorities,
                     )
-                    headerAccessor.setUser { authToken.principal.toString() }
-//                    getAuthentication(
-//                        SecurityContextHolder.getContextHolderStrategy()
-//                    )
-//                    authToken.details = WebAuthenticationDetailsSource().buildDetails(request)
-                    SecurityContextHolder.getContext().authentication = authToken
-
+                    authToken.details = userDetails
+                    headerAccessor.user =  authToken
                 }
             }
         }
--- src/main/kotlin/es/unizar/mii/tmdad/tahc/config/WebSocketConfig.kt
+++ src/main/kotlin/es/unizar/mii/tmdad/tahc/config/WebSocketConfig.kt
@@ -1,14 +1,20 @@
 package es.unizar.mii.tmdad.tahc.config
-import org.apache.catalina.core.ApplicationContext
+
+import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.Message
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver
 import org.springframework.messaging.simp.SimpMessageType
 import org.springframework.messaging.simp.config.ChannelRegistration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.security.authorization.AuthorizationManager
+import org.springframework.security.authorization.SpringAuthorizationEventPublisher
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.context.SecurityContextHolderStrategy
 import org.springframework.security.messaging.access.intercept.AuthorizationChannelInterceptor
 import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager
+import org.springframework.security.messaging.context.AuthenticationPrincipalArgumentResolver
+import org.springframework.security.messaging.context.SecurityContextChannelInterceptor
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
@@ -17,10 +23,14 @@ import org.springframework.web.socket.server.support.DefaultHandshakeHandler
 
 @Configuration
 @EnableWebSocketMessageBroker
-class WebSocketConfig(private val chatPreHandler: ChatPreHandler): WebSocketMessageBrokerConfigurer {
+class WebSocketConfig(private val authenticationChannelInterceptor: AuthenticationChannelInterceptor,
+                      private val applicationContext: ApplicationContext,
+                        ): WebSocketMessageBrokerConfigurer{
 
-    private val securityContextHolderStrategy = SecurityContextHolder
+    val securityContextHolderStrategy: SecurityContextHolderStrategy = SecurityContextHolder
         .getContextHolderStrategy()
+
+    val securityContextChannelInterceptor = SecurityContextChannelInterceptor()
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/ws")
@@ -32,13 +42,21 @@ class WebSocketConfig(private val chatPreHandler: ChatPreHandler): WebSocketMess
         registry.enableSimpleBroker("/canuto")
     }
 
+    override fun addArgumentResolvers(argumentResolvers: MutableList<HandlerMethodArgumentResolver?>) {
+        val resolver = AuthenticationPrincipalArgumentResolver()
+        resolver.setSecurityContextHolderStrategy(securityContextHolderStrategy)
+        argumentResolvers.add(resolver)
+    }
 
     override fun configureClientInboundChannel(registration: ChannelRegistration) {
-        val myAuthorizationRules = messageAuthorizationManager(MessageMatcherDelegatingAuthorizationManager.Builder())
-        val authz = AuthorizationChannelInterceptor(myAuthorizationRules)
-        registration.interceptors(chatPreHandler,authz)
-        super.configureClientInboundChannel(registration);
+        val manager = messageAuthorizationManager(MessageMatcherDelegatingAuthorizationManager.builder())
+        val interceptor = AuthorizationChannelInterceptor(manager)
+        interceptor.setAuthorizationEventPublisher(SpringAuthorizationEventPublisher(applicationContext))
+        interceptor.setSecurityContextHolderStrategy(securityContextHolderStrategy)
+        securityContextChannelInterceptor.setSecurityContextHolderStrategy(securityContextHolderStrategy)
+        registration.interceptors(authenticationChannelInterceptor, securityContextChannelInterceptor, interceptor)
     }
+
 
     fun messageAuthorizationManager(messages: MessageMatcherDelegatingAuthorizationManager.Builder): AuthorizationManager<Message<*>> {
         messages
@@ -47,7 +65,10 @@ class WebSocketConfig(private val chatPreHandler: ChatPreHandler): WebSocketMess
                 SimpMessageType.UNSUBSCRIBE).permitAll()
             .simpSubscribeDestMatchers("/canuto").authenticated()
             .anyMessage().permitAll()
-        return messages.build();
+        return messages.build()
     }
 
 }
+
+
+
