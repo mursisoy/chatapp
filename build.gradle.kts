c82e4a4641df35ff08a8411d89bcec14717fef6f
LorenaM22 <77121705+LorenaM22@users.noreply.github.com>
2023-04-27 18:12:53 +0200

    Añadir las dependencias para rabbit

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,11 @@ dependencies {
 
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
-    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")    
+    
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 
     runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("com.h2database:h2")
