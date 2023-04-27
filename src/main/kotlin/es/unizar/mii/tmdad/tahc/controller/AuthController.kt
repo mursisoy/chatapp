e17502d4d4e531ab38031228a0d50dc3634ffbcf
LorenaM22 <77121705+LorenaM22@users.noreply.github.com>
2023-04-27 18:17:31 +0200

    Cambios para incluir el rabbit
    Cuando un usuario se registra se crea su exchange y queue, y se hace el biding
    Cuando un usuario se logea se activa su consumidor
    Cuando un usuario hace logout se desactiva el consumidor (se cambiara segun Alberto para hacerlo en el disconnect)
    Se incluye el PostMapping de send simplemente por tener el codigo necesario para enviar mensajes por una cola

--- src/main/kotlin/es/unizar/mii/tmdad/tahc/controller/AuthController.kt
+++ src/main/kotlin/es/unizar/mii/tmdad/tahc/controller/AuthController.kt
@@ -12,7 +12,8 @@ import es.unizar.mii.tmdad.tahc.service.AuthenticationService
 @RestController
 @RequestMapping("/api/v1/auth")
 class AuthController(
-    private val authenticationService: AuthenticationService
+    private val authenticationService: AuthenticationService,
+    private val channel: Channel
 ) {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
@@ -25,6 +26,18 @@ class AuthController(
     fun register(
         @RequestBody registerRequest: RegisterRequest
     ): ResponseEntity<AuthenticationResponse> {
+        //Cuando un usuario se registra en nuestro sistema le creamos el exchange
+
+        val exchangeName=registerRequest.username+"Exchange"
+        //realmente seria idUserExchange
+        val queueName=registerRequest.username+"Queue"
+        //sera el idUserQueue
+        val routingKey=registerRequest.username
+        //sera el idUser
+        channel.exchangeDeclare(exchangeName, "direct", true);
+        channel.queueDeclare(queueName, true, false, false, null);
+        channel.queueBind(queueName, exchangeName, routingKey);
+
         return ResponseEntity.ok(authenticationService.register(registerRequest))
     }
 
@@ -32,6 +45,55 @@ class AuthController(
     fun login(
         @RequestBody authenticationRequest: AuthenticationRequest
     ): ResponseEntity<AuthenticationResponse> {
+        //Cuando un usuario se conecta al sistema se crea el topic se hace el biding y se activa el consumidor
+
+        val queueName=authenticationRequest.username+"Queue"
+        //sera el idUser
+
+        val autoAck = false;
+
+        val consumerTag=authenticationRequest.username
+        //ser idUser-tag
+
+        channel.basicConsume(queueName, autoAck, consumerTag,
+            object : DefaultConsumer(channel) {
+                @Throws(IOException::class)
+                override fun handleDelivery(
+                    consumerTag: String?,
+                    envelope: Envelope,
+                    properties: AMQP.BasicProperties,
+                    body: ByteArray?
+                ) {
+                    val routingKey = envelope.routingKey
+                    val contentType = properties.contentType
+                    val deliveryTag = envelope.deliveryTag
+                    // (process the message components here ...)
+                    channel.basicAck(deliveryTag, false)
+
+                }
+            })
+
         return ResponseEntity.ok(authenticationService.login(authenticationRequest))
     }
+    
+    
+    @PostMapping("/logout")
+    fun logout(
+        @RequestBody authenticationRequest: AuthenticationRequest
+    ) {
+        val consumerTag=authenticationRequest.username
+        //ser idUser-tag
+        channel.basicCancel(consumerTag);
+    }
+
+    @PostMapping("/send")
+    fun sendMessage() {
+        val exchangeName="exchange"
+        //realmente seria idUser-exchange
+        val routingKey="foo.bar"
+
+        val messageBodyBytes = "Hello, world!".toByteArray()
+        channel.basicPublish(exchangeName, routingKey, null, messageBodyBytes)
+    }
+
 }
